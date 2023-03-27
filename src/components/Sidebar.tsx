e261d5383f561ff7376f69a96f6c92928820fa82
Felipe Eduardo <Felipeeduardol7@outlook.com>
2023-03-27 16:15:33 -0300

    :tada: init: basic next & tailwind project

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,42 @@
+import { Logo } from "./icons/Logo";
+import { NavLink } from "./NavLink";
+
+export function Sidebar() {
+  return (
+    <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 p-6 hidden lg:block">
+      <Logo className="text-white h-4 w-auto"/>
+
+      <nav className="mt-10 flex flex-col gap-6">
+        <div className="flex flex-col gap-4">
+          <strong className="text-zinc-100 font-medium text-sm">
+            Guides
+          </strong>
+
+          <div className="text-sm flex flex-col pl-2">
+            <NavLink>Introduction</NavLink>
+            <NavLink>Quickstart</NavLink>
+            <NavLink>SDKs</NavLink>
+            <NavLink>Authentication</NavLink>
+            <NavLink>Pagination</NavLink>
+            <NavLink>Erros</NavLink>
+            <NavLink>Webhooks</NavLink>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <strong className="text-zinc-100 font-medium text-sm">
+            Resources
+          </strong>
+
+          <div className="text-sm flex flex-col pl-2">
+            <NavLink>Contacts</NavLink>
+            <NavLink>Conversations</NavLink>
+            <NavLink>Messages</NavLink>
+            <NavLink>Groups</NavLink>
+            <NavLink>Attachments</NavLink>
+          </div>
+        </div>
+      </nav>
+    </aside>
+  )
+}
